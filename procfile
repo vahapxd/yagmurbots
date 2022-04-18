60dd7d21eab315af7aa1df7a2305e89c903611c9
vahapxd <93426712+vahapxd@users.noreply.github.com>
2022-04-18 20:24:48 +0300

    Add files via upload

---
+++ procfile
@@ -0,0 +1,1 @@
+worker: node rencia.js
---
+++ rencia.js
@@ -0,0 +1,103 @@
+const { Client, MessageEmbed, Collection } = require("discord.js");
+const client = (global.client = new Client({ fetchAllMembers: true }));
+const { readdir } = require("fs");
+const config = require("./config.json");
+const db = require("quick.db");
+const moment = require('moment');
+const ms = require("ms");
+require("moment-duration-format");
+const buttons = require('discord-buttons');
+buttons(client)
+const commands = client.commands = new Collection();
+const aliases = client.aliases = new Collection();
+client.cooldown = new Map();
+client.commandblocked = [];
+
+require("./src/helpers/function")(client);
+
+readdir("./src/commands/", (err, files) => {
+    if (err) console.error(err)
+    files.forEach(f => {
+        readdir("./src/commands/" + f, (err2, files2) => {
+            if (err2) console.log(err2)
+            files2.forEach(file => {
+                let prop = require(`./src/commands/${f}/` + file);
+                console.log(`[Rencia-Supervizor] ${prop.name} yüklendi!`);
+                commands.set(prop.name, prop);
+                prop.aliases.forEach(alias => {
+                    aliases.set(alias, prop.name);
+                });
+            });
+        });
+    });
+});
+
+readdir("./src/events", (err, files) => {
+    if (err) return console.error(err);
+    files.filter((file) => file.endsWith(".js")).forEach((file) => {
+        let prop = require(`./src/events/${file}`);
+        if (!prop.conf) return;
+        client.on(prop.conf.name, prop)
+        console.log(`[Chaos-Supervizor] ${prop.conf.name} yüklendi!`);
+    });
+});
+
+client.on("message", async message => {
+    if (message.content === "!buton-rol" && message.author.id === config.bot.owner) {
+        const Giveaway = new buttons.MessageButton()
+            .setStyle("green")
+            .setLabel("🎁 Çekiliş Katılımcısı")
+            .setID("Giveaway");
+        const Activity = new buttons.MessageButton()
+            .setStyle("green")
+            .setLabel("🎉 Etkinlik Katılımcısı")
+            .setID("Activity");
+
+        message.channel.send(`Merhaba! BUBBLES'a Hoşgeldiniz
+ 
+Çekiliş Katılımcısı alarak **Nitro, Spotify, Netflix ve benzeri çekilişlere katılıp ödül sahibi** olabilirsiniz.
+
+Aşağıda bulunan butonlardan **Etkinlik Katılımcısı alarak konserlerimizden, oyunlarımızdan, ve etkinliklerimizden** faydalanabilirsiniz.
+`,
+            {
+                buttons: [Giveaway, Activity]
+            });
+    }
+
+    if (message.content === "!buton-bilgi" && message.author.id === config.bot.owner) {
+
+        const one = new buttons.MessageButton()
+            .setStyle("gray")
+            .setLabel("I")
+            .setID("one");
+
+        const two = new buttons.MessageButton()
+            .setStyle("gray")
+            .setLabel("II")
+            .setID("two");
+
+        const three = new buttons.MessageButton()
+            .setStyle("gray")
+            .setLabel("III")
+            .setID("three");
+
+        const four = new buttons.MessageButton()
+            .setStyle("gray")
+            .setLabel("IV")
+            .setID("four");
+
+        const five = new buttons.MessageButton()
+            .setStyle("gray")
+            .setLabel("V")
+            .setID("five");
+        message.channel.send("**Merhaba!** \n\n Aşşağıdaki butonlarla etkileşime girerek **Sunucumuzdaki durumunuz hakkında bilgi edinebilirsiniz.** \n\n **1 -** `Sunucumuza daha önceden hangi isimlerle kayıt olduğunuzu kontrol edersiniz.` \n **2 -** `Sunucumuza daha önceden kayıt olup olmadığınızı kontrol edersiniz.` \n **3 -** `Sunucumuzda daha önceden ceza alıp almadığınızı kontrol edersiniz.` \n **4 -** `Sunucumuzdaki rollerinizi kontrol edersiniz.` \n **5 -** `Sunucumuza ne zaman katıldığınızı kontrol edersiniz.`", { buttons: [one, two, three, four, five] })
+    }
+});
+
+client.login("OTY1NjM4NDY0NDU0NDA2MjE1.Yl2G5g.cL27_xOrs6HNQMBxvgGTyu7aBJQ").then(x => console.log(`Bot ${client.user.username} olarak giriş yaptı!`)).catch(err => console.log(`Bot Giriş yapamadı sebep: ${err}`));
+
+client.on('message', async message => {
+if (message.content === 'chaos!fakekatıl') { 
+  client.emit('guildMemberAdd', message.member || await message.guild.fetchMember(message.author));
+    }
+});
